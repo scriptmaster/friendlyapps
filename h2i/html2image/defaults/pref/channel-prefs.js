7512c8fa7cf7c3c4ceeee87075a83cead47a9cb9
binja <binja@force.dreamhost.com>
2011-06-26 04:50:56 -0700

    Removed h2i

--- h2i/html2image/defaults/pref/channel-prefs.js
+++
@@ -1,2 +0,0 @@
-//@line 2 "/builds/work/192src/browser/app/profile/channel-prefs.js"
-pref("app.update.channel", "default");
--- h2i/html2image/defaults/pref/firefox-l10n.js
+++
@@ -1,5 +0,0 @@
-//@line 36 "/builds/work/192src/browser/locales/en-US/firefox-l10n.js"
-
-//@line 38 "/builds/work/192src/browser/locales/en-US/firefox-l10n.js"
-
-pref("general.useragent.locale", "en-US");
